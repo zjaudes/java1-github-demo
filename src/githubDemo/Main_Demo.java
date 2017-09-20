9e0cdd89a4d037bbcc128ae5b13444ca04d1822d
zjaudes <zjaudes@MCBA202-C18.classrooms.stlcc>
2017-09-20 11:01:02 -0500

    Initial commit

---
+++ src/githubDemo/Main_Demo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubDemo;
+
+/**
+ *
+ * @author zjaudes
+ */
+public class Main_Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
